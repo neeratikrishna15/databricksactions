aaa7c89fc292ae3a60b82f90d0567e46013859a0
neeratikrishna15 <neerat.krishna15@gmail.com>
2024-01-24 02:26:26 +0530

    changed subnet name

--- vm.tf
+++ vm.tf
@@ -30,7 +30,7 @@ resource "azurerm_network_interface" "example" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "testsubnet"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.public_ip.id
