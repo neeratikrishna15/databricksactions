ce4b91921b7a4d5f4eb93147fb676b3d9d450761
nkrish52_uhg <gopala.krishna@optum.com>
2024-03-25 14:51:09 +0000

    removed secret

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ provider "azurerm" {
   features  {}
   client_id                   = "485f45e2-a017-4b10-bd25-f1476ea05be1"
   #client_certificate_password = var.client_certificate_passwordsdfdfd
-  client_secret    = "5_V8Q~YMSZUR7-ZN61JOjKLWrzalXewcwQ6Y5b_8"
   tenant_id                   = "051f26d8-2bfb-447a-a6b2-d04a33c5afa6"
   subscription_id             = "ac7b009e-5876-4ffe-8832-73ad9124d63f"
 }
